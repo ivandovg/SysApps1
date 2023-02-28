aba353ee87a43b763de153831a72dcd522107294
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2023-02-28 20:48:07 +0200

    Добавьте файлы проекта.

---
+++ SysApps1/FunctionsExt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace SysApps1
+{
+    internal static class FunctionsExt
+    {
+        #region constants
+        // To indicate the buttons displayed in the message box, specify one of the following values.
+        public const int MB_OK = 0x00000000;
+        public const int MB_OKCANCEL = 0x00000001;
+        public const int MB_YESNO = 0x00000004;
+
+        // To display an icon in the message box, specify one of the following values.
+        public const int MB_ICONEXCLAMATION = 0x00000030;
+        public const int MB_ICONWARNING = 0x00000030;
+        public const int MB_ICONINFORMATION = 0x00000040;
+        public const int MB_ICONQUESTION = 0x00000020;
+        public const int MB_ICONHAND = 0x00000010;
+
+        // Controls how the window is to be shown
+        public const int SW_HIDE = 0;
+        public const int SW_SHOWNORMAL = 1;
+        public const int SW_SHOWMINIMIZED = 2;
+        public const int SW_SHOWMAXIMIZED = 3;
+        public const int SW_SHOW = 5;
+        #endregion
+
+
+        #region functions
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto, EntryPoint = "MessageBox")]
+        public static extern int MessageBoxUnicode(IntPtr hWnd, string text, string caption, uint type);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        #endregion
+    }
+}
---
+++ SysApps1/Program1_1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysApps1
+{
+    internal class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "System Applicatins - 1";
+            //TestMessageBox();
+            //TestShowWindow();
+            TestGetindowText();
+
+            Console.WriteLine("press ENTER key...");
+            Console.ReadLine();
+        }
+
+        static void TestMessageBox()
+        {
+            // тест функции MessageBox
+            int result = FunctionsExt.MessageBoxUnicode(IntPtr.Zero, "Hello world!!!", "Test message",
+                FunctionsExt.MB_YESNO | FunctionsExt.MB_ICONQUESTION);
+
+            Console.WriteLine($"result = {result}");
+
+            result = FunctionsExt.MessageBoxUnicode(IntPtr.Zero, "Hello world!!!", "Test message",
+                FunctionsExt.MB_OKCANCEL | FunctionsExt.MB_ICONHAND);
+
+            Console.WriteLine($"result = {result}");
+
+            result = FunctionsExt.MessageBoxUnicode(IntPtr.Zero, "Hello world!!!", "Test message",
+                FunctionsExt.MB_OK | FunctionsExt.MB_ICONINFORMATION);
+
+            Console.WriteLine($"result = {result}");
+        }
+
+        private static void TestShowWindow()
+        {
+            FunctionsExt.ShowWindow((IntPtr)0x00121000, FunctionsExt.SW_HIDE);
+            Console.WriteLine("press ENTER to show window");
+            Console.ReadLine();
+            FunctionsExt.ShowWindow((IntPtr)0x00121000, FunctionsExt.SW_SHOW);
+        }
+
+        private static void TestGetindowText()
+        {
+            StringBuilder sb = new StringBuilder();
+            int h;
+            do
+            {
+                Console.Write("Enter handle: ");
+                h = int.Parse(Console.ReadLine(), System.Globalization.NumberStyles.HexNumber);
+                int len = FunctionsExt.GetWindowText((IntPtr)h, sb, 1024);
+                Console.WriteLine($"String length (answer): {len}\nString: {sb}");
+            } while (h != -1);
+            //Console.ReadLine();
+        }
+    }
+}
